bf4f62cefeb2b43236deb0382848d1cf1f2cca4c
Priyesh Jakhmola <priyeshjakhmola@hotmail.com>
2021-07-28 17:35:21 +0530

    added war

--- src/main/java/com/zensar/AWSDeployment/AwsDeploymentApplication.java
+++ src/main/java/com/zensar/AWSDeployment/AwsDeploymentApplication.java
@@ -2,12 +2,20 @@ package com.zensar.AWSDeployment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsDeploymentApplication {
+public class AwsDeploymentApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsDeploymentApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
 	}
 
 }
